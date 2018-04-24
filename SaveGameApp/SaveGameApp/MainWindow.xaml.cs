8eaff444892b1eeab2d923afd26ace82b5dbecd4
Thomas van Opstal <opstal.thomas@gmail.com>
2018-04-25 00:45:18 +0200

    google api intergration

--- SaveGameApp/SaveGameApp/MainWindow.xaml.cs
+++ SaveGameApp/SaveGameApp/MainWindow.xaml.cs
@@ -15,6 +15,11 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.Xml;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 
 namespace SaveGameApp {
     /// <summary>
@@ -34,8 +39,6 @@ namespace SaveGameApp {
         //Textbox Data
         private const float textBoxHeight = 20.0f;
         private const float textBoxWidth = 500.0f;
-        private const string defaultTextboxText = "Data Path";
-        private const string textboxNamePrefix = "Textbox_";
         #endregion
 
         #region Save/Load Settings
@@ -48,35 +51,36 @@ namespace SaveGameApp {
         private XmlDocument xmlDoc;
         #endregion
 
+        int i = 0;
+
         public MainWindow() {
             InitializeComponent();
-
             RunSetup();
         }
 
         private void RunSetup() {
             xmlPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, xmlFileName);
 
-            if (!File.Exists(xmlPath)) {
+            if (!System.IO.File.Exists(xmlPath)) {
                 string templatePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, templateXmlFileName);
                 XmlDocument saveXml = new XmlDocument();
                 saveXml.Load(templatePath);
                 saveXml.Save(xmlPath);
 
-                CreateButtonGrid(saveXml);
+                DrawButtonGrid(saveXml);
             } else {
                 XmlDocument saveXml = new XmlDocument();
                 saveXml.Load(xmlPath);
 
-                CreateButtonGrid(saveXml);
+                DrawButtonGrid(saveXml);
             }
         }
 
-        private void CreateButtonGrid(XmlDocument saveXml) {
+        private void DrawButtonGrid(XmlDocument saveXml) {
+            i = 0;
+            DynamicButtonGrid.Children.Clear();
 
             foreach (XmlNode buttons in saveXml) {
-                int i = 0;
-
                 foreach (XmlNode button in buttons.ChildNodes) {
                     System.Windows.Controls.Button newBtn = new System.Windows.Controls.Button {
                         Content = defaultButtonText,
@@ -98,15 +102,60 @@ namespace SaveGameApp {
                         Text = button.InnerText
                     };
 
+                    System.Windows.Controls.Button removeButton = new System.Windows.Controls.Button {
+                        Content = "Remove",
+                        Width = buttonWidth,
+                        Height = buttonHeight,
+                        HorizontalAlignment = System.Windows.HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Top,
+                        Margin = new Thickness((buttonWidth + textBoxWidth), (i * buttonHeight), 0, 0),
+                        Name = button.Name
+                    };
+
                     DynamicButtonGrid.Children.Add(newBtn);
                     DynamicButtonGrid.Children.Add(newTxb);
+                    DynamicButtonGrid.Children.Add(removeButton);
                     i++;
 
                     newBtn.Click += OnButtonClicked;
+                    removeButton.Click += OnRemoveClicked;
                 }
             }
 
             xmlDoc = saveXml;
+
+            DrawConfigButtons(i);
+        }
+
+        private void DrawConfigButtons(int btnAmount) {
+            //Add Path
+            System.Windows.Controls.Button addPath = new System.Windows.Controls.Button {
+                Content = "Add Path",
+                Width = buttonWidth,
+                Height = buttonHeight,
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Margin = new Thickness(0, (btnAmount * buttonHeight), 0, 0)
+            };
+
+            DynamicButtonGrid.Children.Add(addPath);
+            addPath.Click += AddPath;
+
+            SizeToContent = SizeToContent.Height;
+        }
+
+        private void AddPath(object sender, RoutedEventArgs e) {
+            foreach (XmlNode buttons in xmlDoc) {
+                if (buttons.Name == xmlButtons) {
+                    XmlNode newBtn = xmlDoc.CreateNode("element", buttonNamePrefix + i.ToString(), "");
+                    newBtn.InnerText = "File Path";
+                    buttons.AppendChild(newBtn);
+                    break;
+                }
+            }
+
+            xmlDoc.Save(xmlPath);
+            DrawButtonGrid(xmlDoc);
         }
 
         private void OnButtonClicked(object sender, RoutedEventArgs e) {
@@ -121,8 +170,19 @@ namespace SaveGameApp {
                         button.InnerText = folderBrowseDialog.SelectedPath;
 
             xmlDoc.Save(xmlPath);
+            DrawButtonGrid(xmlDoc);
+        }
 
-            CreateButtonGrid(xmlDoc);
+        private void OnRemoveClicked(object sender, RoutedEventArgs e) {
+            System.Windows.Controls.Button clickedButton = (System.Windows.Controls.Button)sender;
+
+            foreach (XmlNode buttons in xmlDoc)
+                foreach (XmlNode button in buttons.ChildNodes)
+                    if (button.Name == clickedButton.Name)
+                        buttons.RemoveChild(button);
+
+            xmlDoc.Save(xmlPath);
+            DrawButtonGrid(xmlDoc);
         }
     }
 }
